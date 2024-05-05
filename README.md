0b9443e878ac5db1cfb88ae97e0c16c17eda8c3b
Prashant Agrawal <prashantagrawal2107@gmail.com>
2024-05-05 20:50:58 +0530

    done

---
+++ README.md
@@ -0,0 +1,6 @@
+This is a React Single Page App.
+This a a simple e-comm app.
+
+This is to understand React Router , Context API and to understand other concepts of React.
+
+THANK YOU SO MUCH !!
---
+++ src/config/firebase.js
@@ -0,0 +1,33 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import {
+  getAuth,
+  setPersistence,
+  browserLocalPersistence,
+} from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  // Get your Own Configuration....
+  // You need to set up your own firebase and add products to the database....
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
+};
+
+console.log(process.env);
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth();
+setPersistence(auth, browserLocalPersistence);
+export { db };
